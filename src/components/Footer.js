5e3ede0d3eb6f4c46a586b4fab42024f1ef38f08
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2023-01-28 14:34:41 +0200

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { 
+    AiFillLinkedin, 
+    AiFillGithub, 
+    AiFillCodepenCircle, 
+    AiFillBehanceCircle 
+} from "react-icons/ai";
+import signature from "../images/signature.png";
 
 export default function Footer() {
     return (
-        <div className="footer">
-            <div className="top-content">
-                <div>My Reads</div>
-                <div>  
-                    <Link to="/search">Search</Link>
-                    <Link to="/">Home</Link>
-                </div>
+        <footer className="footer">
+			<div>
+                All Rights reserved &copy; {new Date().getFullYear()}, <a href="https://github.com/Mohamed-Elhawary/my-reads" target="_blank">My Reads</a> {/* eslint-disable-line */}
+                <p className="author">
+                    <div>
+                        Made by: 
+                        <a href="https://www.linkedin.com/in/mohamed-elhawary14/" target="_blank"> {/* eslint-disable-line */} 
+                            <img src={signature} alt="signature" width="100" height="40" />
+                        </a>
+                    </div>
+                </p> 
+                <ul className="social">
+                    <li className="mr-3"><a href="https://www.linkedin.com/in/mohamed-elhawary14/" target="_blank"><AiFillLinkedin size={30} /></a></li> {/* eslint-disable-line */}
+                    <li className="mr-3"><a href="https://github.com/Mohamed-Elhawary" target="_blank"><AiFillGithub size={30} /></a></li> {/* eslint-disable-line */}
+                    <li className="mr-3"><a href="https://codepen.io/Mohamed-ElHawary" target="_blank"><AiFillCodepenCircle size={30} /></a></li> {/* eslint-disable-line */}
+                    <li className="mr-3"><a href="https://www.behance.net/mohamed-elhawary14" target="_blank"><AiFillBehanceCircle size={30} /></a></li> {/* eslint-disable-line */}
+                </ul>
             </div>
-            <div className="bottom-content">
-                <p>All Rights are Reserved &copy; {new Date().getFullYear()}</p>
-                <p>Made by <a href="https://github.com/Mohamed-Elhawary" target="_blank">HaWaRy</a></p>
-            </div>
-        </div>
+		</footer>
     )
 }
